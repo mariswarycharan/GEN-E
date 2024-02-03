322788a646ee9a4f48fc34b0a04ffce60889fc4c
Charan A A <104826220+mariswarycharan@users.noreply.github.com>
2024-02-03 15:59:00 +0530

    Update Buddy.py

--- pages/Buddy.py
+++ pages/Buddy.py
@@ -8,9 +8,9 @@ from langdetect import detect
 st.set_page_config(page_title="Gen-e",page_icon="🤖")
 hide_st_style = """
             <style>
-            # MainMenu {visibility: hidden;}
+            MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            # header {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
