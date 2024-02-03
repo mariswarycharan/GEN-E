c54d901dd994ee4c7162f913cd433618b18be507
Charan A A <104826220+mariswarycharan@users.noreply.github.com>
2024-02-03 16:00:12 +0530

    Update Library.py

--- pages/Library.py
+++ pages/Library.py
@@ -6,9 +6,9 @@ import os
 st.set_page_config(page_title="Gen-e",page_icon="🤖")
 hide_st_style = """
             <style>
-            # MainMenu {visibility: hidden;}
+            MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            # header {visibility: hidden;}
+            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
